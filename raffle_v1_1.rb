590464a22bb9584f6486706d631956ed86c465c1
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-30 16:14:50 -0500

    5 sucessful tests

--- raffle_v1_1.rb
+++ raffle_v1_1.rb
@@ -1,9 +1,12 @@
 def one_off(winning_num, ticket_num)
-
+# sets matches equal to zero
 	matches = 0
+# sets index_position equal to zero
 	index_position = 0
 
+#iterates the number of times as long as the length of ticket_num.
 	ticket_num.length.times do 
+# condition: if the index position of ticket_num is equal to the index position of winning_num
 		if ticket_num[index_position] == winning_num[index_position]
 			matches = matches + 1
 		end
@@ -36,3 +39,20 @@ end
 
 	#if ticket_num.match[0][1][2]winning_num[]
 #end
+
+def returning_function( my_num, win_nums)
+
+	array = []
+
+	win_nums.each do |win_num|
+
+		if one_off(win_num,my_num)
+
+		true
+
+			array << win_num
+			
+		end
+	end
+	array
+end
--- test_raffle_v1_1.rb
+++ test_raffle_v1_1.rb
@@ -20,3 +20,18 @@ class Test_raffle_function < MiniTest::Test
 		assert_equal(true, results)
 	end
 end
+
+ class Test_return_raffle < MiniTest::Test
+
+ 	def test_return_empty_array
+ 		my_num = '1234'
+ 		win_nums = ['4567', '9874', '9999']
+ 		assert_equal([], returning_function(my_num, win_nums))
+ 	end
+
+ 	def test_return_array_of_1
+ 		my_num = '1234'
+ 		win_nums = ['3234', '5555', '6789']
+ 		assert_equal(['3234'], returning_function(my_num, win_nums))
+ 	end
+ end
