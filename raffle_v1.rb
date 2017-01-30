1932e9db600e775770fd68fa443a2ee9d2ca925b
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-30 11:39:59 -0500

    4 working tests

--- raffle_v1.rb
+++ raffle_v1.rb
@@ -1,1 +1,14 @@
-check to see if a number (value) exists in an array.
+#check to see if a number (value) exists in an array.
+
+def raffle_function(ticket_num, winning_num_array)
+	#winning_num_array.include?(ticket_num)
+
+	#or
+	
+	if winning_num_array.include?(ticket_num)
+		true
+	else
+		false
+	end
+
+end
---
+++ test_raffle_v1.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require_relative 'raffle_v1.rb'
+
+class Raffle_class < Minitest::Test
+
+	def test_1_equals_1()
+		assert_equal(1,1)
+	end
+
+	def	test_number_exists()
+		ticket_num = '1'
+		winning_num_array = []
+		assert_equal(false, raffle_function(ticket_num, winning_num_array))
+	end
+	def test_number_returns_true()
+		ticket_num = '1001'
+		winning_num_array = ['1000', '1111', '1001']
+		# assert_equal 
+		assert_equal(true, raffle_function(ticket_num, winning_num_array))
+	end
+	def test_number_returns_true_iteration
+		ticket_num = '1112'
+		winning_num_array = ['1000', '1111', '1001']
+		assert_equal(false, raffle_function(ticket_num, winning_num_array))
+	end
+end 
+
