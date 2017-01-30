349bdc840a68d1560e76646e9f48086907e3a12c
April Blair <a.m.blair@eagle.clarion.edu>
2017-01-30 11:51:41 -0500

    5 passing tests

--- raffle_v1.rb
+++ raffle_v1.rb
@@ -4,8 +4,20 @@ def raffle_function(ticket_num, winning_num_array)
 	#winning_num_array.include?(ticket_num)
 
 	#or
-	
-	if winning_num_array.include?(ticket_num)
+	# winning_num_array.each do |ticket|
+	# if winning_num_array.include?(ticket_num)
+	# 	true
+	# else
+	# 	false
+	# end
+	counter = 0
+	winning_num_array.each do |ticket|
+		
+		if ticket_num == ticket
+			counter = counter + 1
+		end
+	end
+	if counter > 0
 		true
 	else
 		false
--- test_raffle_v1.rb
+++ test_raffle_v1.rb
@@ -23,5 +23,10 @@ class Raffle_class < Minitest::Test
 		winning_num_array = ['1000', '1111', '1001']
 		assert_equal(false, raffle_function(ticket_num, winning_num_array))
 	end
-end 
+	def test_returns_true
+		ticket_num = '1234'
+		winning_num_array = ['1234']
+		assert_equal(true, raffle_function(ticket_num, winning_num_array))
+	end 
+end
 
